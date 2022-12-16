528c6fee8d210bfcef889f0516c11c9cd37f6116
Aleksei Tupikov <a.tupikov.miel@gmail.com>
2022-12-16 17:38:28 +0100

    added method describing

--- src/test/java/guru/qa/tests/StudentRegistrationFormTest.java
+++ src/test/java/guru/qa/tests/StudentRegistrationFormTest.java
@@ -1,13 +1,22 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static guru.qa.tests.TestData.*;
 
 public class StudentRegistrationFormTest extends TestBase {
 
     @Test
+    @Feature("Student Registration form")
+    @Story("Sending Student Registration form with all fields")
+    @Severity(SeverityLevel.BLOCKER)
+    @DisplayName("E2E sending form test")
     void sendFormTest() {
 
         registrationPage.openPage()
